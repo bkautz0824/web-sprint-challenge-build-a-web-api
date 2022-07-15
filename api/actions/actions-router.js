1fa11d9c816707ff7ed568c13563f73dc9999fff
Bennett Kautz <bennettkautz@gmail.com>
2022-07-15 18:55:21 -0400

    router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,26 @@
 // Write your "actions" router here!
+const express = require('express')
+const { validateActionId } = require('./actions-middlware')
+const Actions = require('./actions-model')
+
+const router = express.Router()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
-// Write your "projects" router here!
+
+const express = require('express')
+const { validateProjectId, validateProject } = require('./projects-middleware')
+const Projects = require('./projects-model')
+
+
+
+const router = express.Router()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
